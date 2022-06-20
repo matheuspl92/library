749e1004f7eccff0ffab06c6017851f1765a4e11
Matheus <matheuspl92@hotmail.com>
2022-06-20 18:44:23 -0300

    Add functions that show each book info on the page

--- script.js
+++ script.js
@@ -20,3 +20,35 @@ addBookToLibrary("Starship Troopers", "Heinlein", 400, true);
 addBookToLibrary("Democracia, o deus que falhou", "Hayke", 500, false);
 
 console.log(myLibrary);
+
+function createCard(book){
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("card");
+
+    const bookName = document.createElement("h3");
+    const bookAuthor = document.createElement("h4");
+    const bookPages = document.createElement("p");
+    const wasBookRead = document.createElement("p");
+
+    bookName.innerHTML = book.bookName;
+    bookAuthor.innerHTML = book.bookAuthor;
+    bookPages.innerHTML = book.bookPages;
+    wasBookRead.innerHTML = book.wasBookRead;
+
+    cardContainer.appendChild(bookName);
+    cardContainer.appendChild(bookAuthor);
+    cardContainer.appendChild(bookPages);
+    cardContainer.appendChild(wasBookRead);
+
+    return cardContainer;
+}
+
+function showLibrary() {
+    const container = document.querySelector(".container");
+    
+    myLibrary.forEach(book => {
+        container.appendChild(createCard(book));
+    });
+}
+
+showLibrary();
